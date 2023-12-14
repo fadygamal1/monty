24256343d6678b1cbb12eee13e24a707a391fa28
fadygamal11 <fadygamal007@gmail.com>
2023-12-14 02:57:48 -0800

    commit

--- main.c
+++ main.c
@@ -1,47 +1,83 @@
 #include "monty.h"
-
-input_t var = {NULL, NULL};
+stack_t *head = NULL;
 
 /**
- * main - Main function that calls other functions in the program
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the arguments
- *
- * Return: 0 on success, non-zero on failure
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
-int main(int argc, char **argv)
-{
-	FILE *fp;
-	char line[128];
-	int line_nbr = 0;
-	stack_t *stack = NULL;
 
+int main(int argc, char *argv[])
+{
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	fp = fopen(argv[1], "r");
-
-	if (fp == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	while (fgets(line, 128, fp))
-	{
-		line_nbr++;
-		var.op = strtok(line, "\n ");
-		if (strcmp(var.op, "push") == 0)
-			var.nbr = strtok(NULL, " ");
-		interprete(&stack, line_nbr);
-	}
-
-	if (stack != NULL)
-		free_stack(stack);
-
-	fclose(fp);
+	open_file(argv[1]);
+	free_nodes();
 	return (0);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
--- monty.h
+++ monty.h
@@ -1,11 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#define _GNU_SOURCE
 #include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <unistd.h>
-#include <sys/stat.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -13,16 +15,15 @@
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
- * Decription: doubly linked list node structure
+ * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
-
 
 /**
  * struct instruction_s - opcode and its function
@@ -34,40 +35,50 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
 
-/**
- * struct monty_instance - variables needed across multiple file
- *
- * @file: pointer to the monty file
- * @monty_arg: opcode argument
- * @line: the content of the line
- *
- * Description: holds values needed in multiple files of the program
- */
-typedef struct monty_instance
-{
-	FILE *file;
-	char *monty_arg;
-	char *line;
-} monty_inst;
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
 
-extern monty_inst instance_vars;
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void add_to_stack(stack_t **, unsigned int);
+void print_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
 
-int execute(stack_t **stack, unsigned int line_number, char *content,
-		FILE *file);
-void stack_push(stack_t **stack, unsigned int line_number);
-void free_struct(stack_t *head);
-void struct_pall(stack_t **top, unsigned int line_number);
-void pint_top(stack_t **stack, unsigned int line_number);
-void pop_top(stack_t **stack, unsigned int line_number);
-void swap_elem(stack_t **stack, unsigned int line_number);
-void add_elem(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub_elem(stack_t **stack, unsigned int line_number);
-void div_elem(stack_t **stack, unsigned int line_number);
+void call_fun(op_func, char *, char *, int, int);
 
-#endif /* MONTY_H */
+void pop_top(stack_t **, unsigned int);
+void print_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+/*String operations*/
+void rotl(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void print_char(stack_t **, unsigned int);
+
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+void string_err(int error_code, ...);
+
+#endif
